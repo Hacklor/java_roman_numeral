f1d26aa1aa51b1df656e949a37cba4b924e16daa
Nicole Bekkers <nicole@kabisa.nl>
2017-08-04 12:18:15 +0200

    Remove first test
    It has the same intention as the second.

--- src/nl/kabisa/the_guild/roman_numeral/RomanNumeralTest.java
+++ src/nl/kabisa/the_guild/roman_numeral/RomanNumeralTest.java
@@ -1,14 +1,8 @@
 package nl.kabisa.the_guild.roman_numeral;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class RomanNumeralTest {
-    @Test
-    public void test() {
-        assertTrue(true);
-    }
 
     @Test
     public void createsMethod() {
