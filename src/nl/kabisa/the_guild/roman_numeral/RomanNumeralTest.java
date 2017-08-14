2c3ca5fc1d4a877a87a2cc14a6dc1a2a5532090a
Nicole Bekkers <nicole@kabisa.nl>
2017-08-14 11:24:13 +0200

    GREEN: Adding other digits to map.

--- src/nl/kabisa/the_guild/roman_numeral/RomanNumeralTest.java
+++ src/nl/kabisa/the_guild/roman_numeral/RomanNumeralTest.java
@@ -25,9 +25,14 @@ public class RomanNumeralTest {
     private static Map<String,Integer> romanToArabicMap() {
         Map<String, Integer> romanToArabic = new HashMap<>();
 
-        romanToArabic.put("X", 10);
-        romanToArabic.put("V", 5);
+
         romanToArabic.put("I", 1);
+        romanToArabic.put("V", 5);
+        romanToArabic.put("X", 10);
+        romanToArabic.put("L", 50);
+        romanToArabic.put("C", 100);
+        romanToArabic.put("D", 500);
+        romanToArabic.put("M", 1000);
 
         return romanToArabic;
     }
